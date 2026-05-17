0acb2c4bcd31d4836624621cb1aac32e33dc0a6e
angelojr182 <miguelescoto182@gmail.com>
2026-05-17 01:29:40 -0600

    fix: incluir secrets.js para GitHub Pages (anon key es pública por diseño)

---
+++ secrets.js
@@ -0,0 +1,4 @@
+const SECRETS = {
+  SURL: 'https://oxhzxistgyfvkhzncxpz.supabase.co',
+  SKEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94aHp4aXN0Z3lmdmtoem5jeHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NTE0NzIsImV4cCI6MjA5MzIyNzQ3Mn0.-Q8XybmgAQdiBA5tAFi9LLoFnLuF0nJlsCh8WlTUjis'
+};
